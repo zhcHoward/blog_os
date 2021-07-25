3bf2fa7bed36c0f8991502234a6fa57eba122bad
Howard Zhu <zhc_howard@hotmail.com>
2021-07-25 14:39:59 +0800

    finish Testing

--- tests/basic_root.rs
+++ tests/basic_root.rs
@@ -3,9 +3,10 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use blog_os::println;
 use core::panic::PanicInfo;
 
 #[no_mangle] // don't mangle the name of this function
@@ -15,11 +16,12 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-fn test_runner(tests: &[&dyn Fn()]) {
-    unimplemented!();
-}
-
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    loop {}
+    blog_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
